92193b7e2f4e6cf49489a6830d41b40763b62171
J-Mehmood <302636056@student.rocmondriaan.nl>
2023-06-15 15:18:16 +0200

    projecten js toegevoegd

---
+++ js/projecten.js
@@ -0,0 +1,76 @@
+let click = document.querySelector('.click');
+
+click.addEventListener("click", plekFunction)
+
+
+
+
+function plekFunction() {
+
+    window.location.href = `HL/index.html`
+
+};
+
+
+
+
+let click2 = document.querySelector('.click2');
+
+click2.addEventListener("click", plekFunction2)
+
+
+
+
+function plekFunction2() {
+
+    window.location.href = `Escape room spel - kopie/welcome.html`
+
+};
+
+
+
+
+let click3 = document.querySelector('.click3');
+
+click3.addEventListener("click", plekFunction3)
+
+
+
+
+function plekFunction3() {
+
+    window.location.href = `boterkaas/boter.html`
+
+};
+
+
+
+
+let click4 = document.querySelector('.click4');
+
+click4.addEventListener("click", plekFunction4)
+
+
+
+
+function plekFunction4() {
+
+    window.location.href = `vtrend/index.html`
+
+};
+
+
+
+
+let click5 = document.querySelector('.click5');
+
+click5.addEventListener("click", plekFunction5)
+
+
+
+
+function plekFunction5() {
+
+    window.location.href = `wack/index.html`
+
+};
